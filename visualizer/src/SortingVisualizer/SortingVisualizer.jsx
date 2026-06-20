761f5d0ef591dc00339e2b3da1b163b102ccb8fc
braa-oudeh <braaoudeh@arizona.edu>
2026-06-21 02:34:56 +0400

    white spaces were killing me

--- visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,21 +2,16 @@ import { useState } from 'react'
 import { mergeSort, mergeLists } from '../SortingAlgorithms/mergeSort.js';
 export default function SortingVisualizer() {
     const [arr, setArr] = useState([]);
-
     function genereateNewArray()
     {
         let temp = [];
-        
         for (let i = 0; i < 1000; i++)
         {
             temp.push(getRandomInt(5,1000));        // starts from 5 because bars less than that are small
         }
-
         setArray(temp);
     }
 }
-
-
 
 // https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 function getRandomInt(min, max) {
