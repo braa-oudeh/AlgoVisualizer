c9b81b622227bdad7949d14d0b82e924ae327f5f
braa-oudeh <braaoudeh@arizona.edu>
2026-06-21 02:32:56 +0400

    generating a random arr

--- visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
 import { mergeSort, mergeLists } from '../SortingAlgorithms/mergeSort.js';
 export default function SortingVisualizer() {
-  return (
-    <>
-    <p>Darling, Has the Count not just told us that all human wisdom was contained in these two words - 'wait' and 'hope'? </p>
-    </>
-  )
+    const [arr, setArr] = useState([]);
+
+    function genereateNewArray()
+    {
+        let temp = [];
+        
+        for (let i = 0; i < 1000; i++)
+        {
+            temp.push(getRandomInt(5,1000));        // starts from 5 because bars less than that are small
+        }
+
+        setArray(temp);
+    }
 }
 
+
+
+// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
