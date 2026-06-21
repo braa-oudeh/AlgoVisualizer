112bd289ddf56af1295d8cf92b9a8fa3c5064332
braa-oudeh <braaoudeh@arizona.edu>
2026-06-21 14:38:18 +0400

    implement binarySearch

---
+++ visualizer/src/SearchingAlgorithms/binarySearch.js
@@ -0,0 +1,25 @@
+export function binarySearch(arr,target)
+{
+    let l = 0;
+    let r = (arr.length) - 1 
+    while (l <= r)
+    { 
+        let mid = Math.floor(l + (r - l) / 2);
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            l = mid + 1;
+        }
+
+        else if (arr[mid] > target)
+        {
+            r = mid - 1;
+        }
+
+    }
+    return -404; // Not found
+}
---
+++ visualizer/tests/SearchingTests/binarySearch.test.js
@@ -0,0 +1,24 @@
+import { binarySearch } from '../../src/SearchingAlgorithms/binarySearch.js'
+import { describe, test } from 'node:test';
+import assert from 'node:assert';
+
+
+test('Search a sorted array for a target', () => {
+  assert.deepStrictEqual(binarySearch([1, 2, 3, 4, 5], 5), 4);
+});
+
+test('handles an empty array', () => {
+  assert.deepStrictEqual(binarySearch([],5), -404);
+});
+
+test('handles a single-element array', () => {
+  assert.deepStrictEqual(binarySearch([7],7), 0);
+});
+
+test('handles middle target in sorted array', () => {
+  assert.deepStrictEqual(binarySearch([1, 2, 3, 4, 5], 3), 2);
+});
+
+test('handles first element target in sorted array', () => {
+  assert.deepStrictEqual(binarySearch([1, 2, 3, 4, 5], 1), 0);
+});
