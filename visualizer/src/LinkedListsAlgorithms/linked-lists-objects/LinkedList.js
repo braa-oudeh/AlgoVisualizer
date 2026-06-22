de5f7cdf0d1638a739bd34a446073ec6f1b65e1b
braa-oudeh <braaoudeh@arizona.edu>
2026-06-22 19:18:04 +0400

    Add delete operation to linked list class

--- visualizer/src/LinkedListsAlgorithms/linked-lists-objects/LinkedList.js
+++ visualizer/src/LinkedListsAlgorithms/linked-lists-objects/LinkedList.js
@@ -16,6 +16,9 @@ export class LinkedList
     push(data)
     {
         const node = new Node(data);
+
+        // check if linked list is empty, if yes then the data is our first element
+
         if (this.head == null)
         {
             this.head = node;
@@ -29,8 +32,41 @@ export class LinkedList
         curr.next = node;
     } 
 
+    delete(data)
+    {
+        // if list is empty, simply exit
+        if (this.head == null)
+        {
+            return;
+        }
+
+        // if deleted element is in the first of the list, simply move the head to the next pointer 
+        else if (this.head.data == data)
+        {
+            this.head = this.head.next; 
+            return;
+        }
+
+        let curr = this.head; 
+        let prev = null;
+        
+        while (curr && curr.data != data)
+        {
+            prev = curr; 
+            curr = curr.next;
+        }
+
+        if (curr)
+        {
+            prev.next = curr.next;
+            curr.next = null;
+        }
+    }
+
     toArray()
     {
+        // function to copy all the nodes to an array for easier testing
+
         const arr = [];
         let curr = this.head;
         while (curr)
@@ -42,6 +78,7 @@ export class LinkedList
     }
     display()
     {
+        // helper method to print the nodes for easier debugging
         let result = "";
         let curr = this.head;
         while (curr)
--- visualizer/tests/LinkedListsAlgorithmsTests/LinkedList.test.js
+++ visualizer/tests/LinkedListsAlgorithmsTests/LinkedList.test.js
@@ -2,14 +2,14 @@ import { LinkedList} from '../../src/LinkedListsAlgorithms/linked-lists-objects/
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
-test('check whether linked List push elements works', () => {
+test('check if linked List push elements works', () => {
     const ll_list = new LinkedList();
     ll_list.push(10);
     const new_arr = ll_list.toArray();
     assert.deepStrictEqual(new_arr, [10]);
 });
 
-test('check whether linked List multiple push elements works', () => {
+test('check if linked List multiple push elements works', () => {
     const ll_list = new LinkedList();
     for (let i = 0; i < 4; i++)
     {
@@ -20,8 +20,63 @@ test('check whether linked List multiple push elements works', () => {
     assert.deepStrictEqual(new_arr, [0,10,20,30]);
 });
 
-test('check whether linked List is empty at start', () => {
+test('check if linked List is empty at start', () => {
     const ll_list = new LinkedList();
     const new_arr = ll_list.toArray();
     assert.deepStrictEqual(new_arr, []);
+});
+
+test('check if linked list delete doesnt break on empty LLs', () => 
+{
+    const ll_list = new LinkedList();
+    ll_list.delete(10);
+    const new_arr = ll_list.toArray();
+    assert.deepStrictEqual(new_arr, []);
+});
+
+test('check if linked list deletion works on a single operation', () => 
+{
+    const ll_list = new LinkedList();
+    ll_list.push(10);
+    ll_list.delete(10);
+    const new_arr = ll_list.toArray();
+    assert.deepStrictEqual(new_arr, []);
+});
+
+test('check if linked list delete works for an element in the middle', () => {
+    const ll_list = new LinkedList();
+    for (let i = 0; i < 4; i++)
+    {
+        ll_list.push(i);
+    }
+    ll_list.delete(2);
+    const new_arr = ll_list.toArray();
+    assert.deepStrictEqual(new_arr,[0,1,3]);
+});
+
+test('check if linked list delete works for an element at end', () => {
+    const ll_list = new LinkedList();
+    for (let i = 0; i < 4; i++)
+    {
+        ll_list.push(i);
+    }
+    ll_list.delete(3);
+    const new_arr = ll_list.toArray();
+    assert.deepStrictEqual(new_arr,[0,1,2]);
+});
+
+test('check if linked list delete works for deleting all elements', () => {
+    const ll_list = new LinkedList();
+    for (let i = 0; i < 4; i++)
+    {
+        ll_list.push(i);
+    }
+    
+    ll_list.delete(0);
+    ll_list.delete(1);
+    ll_list.delete(2);
+    ll_list.delete(3);
+
+    const new_arr = ll_list.toArray();
+    assert.deepStrictEqual(new_arr,[]);
 });
